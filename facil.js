a8f29136389a13ddf12275bf138561a62a8d75fe
Ana Julia Perejon <anajp1008@gmail.com>
2025-06-10 15:35:30 -0400

    Add files via upload

--- facil.js
+++ facil.js
@@ -1,11 +1,30 @@
-const crs = ["facil1.html", "facil2.html", "facil3.html"]
-var i = 0;
+const listaPerguntaFacil = ["facil1.html", "facil2.html", "facil3.html","facil4.html"]
 
-function play(){
-    document.location.href("facil1.html")
-}
+const listaPerguntaDificil = []
 
-function passar() {
-    i--;
-    document.location.href=crs[i]
+const botaoProximo = document.getElementById("botaoPassar");
+
+botaoProximo.style.display="none"
+
+
+const resposta1 = document.getElementById("resposta1")
+const resposta2 = document.getElementById("resposta2")
+const resposta3 = document.getElementById("resposta3")
+const resposta4 = document.getElementById("resposta4")
+
+let listaResposta = [resposta1,resposta2,resposta3,resposta4]
+
+function responder(botaoClicado) {
+
+    for (let resposta of listaResposta) {
+        if (resposta.value=="certo") {
+            resposta.style.backgroundColor = "green"
+        }
+        else {
+            resposta.style.backgroundColor="red"
+        }
+    }
+
+    botaoProximo.style.display=""
+
 }
---
+++ index.js
@@ -0,0 +1,13 @@
+function playFacil() {
+
+    sessionStorage.setItem("dificuldade","facil")
+
+    window.location.href = "facil1.html"
+}
+
+function playDificil() {
+
+    sessionStorage.setItem("dificuldade","dificil")
+
+    window.location.href = "facil1.html"
+}
